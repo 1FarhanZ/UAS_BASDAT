082261ad0e9fce5b57df2dcfb8604c80fe8b6fad
Farhan-netizzen <farhanabob20149@gmail.com>
2021-08-14 20:12:01 +0700

    UAS Basis Data

--- application/views/templates_admin/sidebar.php
+++ application/views/templates_admin/sidebar.php
@@ -7,7 +7,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo base_url('admin/dashboard_admin') ?>">
                 <div class="sidebar-brand-icon r">
                     <i class="fas fa-store"></i>
                 </div>
@@ -40,6 +40,8 @@
                     <span>Invoice</span></a>
             </li>
 
+            
+
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
@@ -49,13 +51,7 @@
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
             </div>
 
-            <!-- Sidebar Message -->
-            <div class="sidebar-card d-none d-lg-flex">
-                <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
-                <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-                <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
-            </div>
-
+           
         </ul>
         <!-- End of Sidebar -->
 
